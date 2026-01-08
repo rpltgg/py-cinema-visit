95f30f37e65f63b5ae038e69332addccd8a978d0
nnoname <nnoname@ex.com>
2026-01-08 15:46:18 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -4,7 +4,12 @@ from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
+def cinema_visit(
+    customers: list,
+    hall_number: int,
+    cleaner: str,
+    movie: str
+):
     customer_objects = []
     for customer in customers:
         customer_obj = Customer(name=customer["name"], food=customer["food"])
