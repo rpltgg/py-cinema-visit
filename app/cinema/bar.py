74b97dcfa00c4c4ff6d7e6724ab8347e8f981845
nnoname <nnoname@ex.com>
2026-01-08 15:52:07 +0100

    Solution

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -1,4 +1,4 @@
 class CinemaBar:
     @staticmethod
-    def sell_product(product, customer):
+    def sell_product(product, customer) -> None:
         print(f"Cinema bar sold {product} to {customer.name}.")
--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,8 +1,8 @@
 class CinemaHall:
-    def __init__(self, hall_number):
+    def __init__(self, hall_number: int) -> None:
         self.hall_number = hall_number
 
-    def movie_session(self, movie_name, customers, cleaning_staff):
+    def movie_session(self, movie_name, customers, cleaning_staff) -> None:
         print(f'"{movie_name}" started in hall number {self.hall_number}.')
         for customer in customers:
             customer.watch_movie(movie_name)
